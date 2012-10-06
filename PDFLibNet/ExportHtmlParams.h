68b12d7b21e5de78ba17881eb70e316b62afb577
Pieter van Ginkel <pvginkel@gmail.com>
2012-10-06 08:33:54 +0200

    Fixed namespace of ExportHtmlParams.

--- PDFLibNet/ExportHtmlParams.h
+++ PDFLibNet/ExportHtmlParams.h
@@ -3,130 +3,133 @@
 using namespace System;
 using namespace System::Runtime::InteropServices;
 
-public ref class ExportHtmlParams
+namespace PDFLibNet
 {
-private:
-	System::Int32 jpegQuality;
-	System::String ^imgExt;
-	System::String ^encName;
-	System::Double zoom;
-	System::Boolean noFrames;
-	System::Boolean complexMode;
-	System::Boolean htmlLinks;
-	System::Boolean ignoreImages; 
-	System::Boolean outputHiddenText;
+	public ref class ExportHtmlParams
+	{
+	private:
+		System::Int32 jpegQuality;
+		System::String ^imgExt;
+		System::String ^encName;
+		System::Double zoom;
+		System::Boolean noFrames;
+		System::Boolean complexMode;
+		System::Boolean htmlLinks;
+		System::Boolean ignoreImages; 
+		System::Boolean outputHiddenText;
 	
-public:
-	ExportHtmlParams()
-	{
-		imgExt = gcnew System::String("png");
-		encName = gcnew System::String("UTF-8");
-		noFrames=false;
-		complexMode = true;
-		htmlLinks = true;
-		ignoreImages = false;
-		outputHiddenText = false;
-		zoom = 1.50;
-		jpegQuality = 85;
-	}
+	public:
+		ExportHtmlParams()
+		{
+			imgExt = gcnew System::String("png");
+			encName = gcnew System::String("UTF-8");
+			noFrames=false;
+			complexMode = true;
+			htmlLinks = true;
+			ignoreImages = false;
+			outputHiddenText = false;
+			zoom = 1.50;
+			jpegQuality = 85;
+		}
 
-	property System::Int32 JpegQuality
-	{
-		System::Int32 get()
+		property System::Int32 JpegQuality
 		{
-			return jpegQuality;
+			System::Int32 get()
+			{
+				return jpegQuality;
+			}
+			void set(System::Int32 value)
+			{
+				jpegQuality = value;
+			}
 		}
-		void set(System::Int32 value)
+		property System::Double Zoom
 		{
-			jpegQuality = value;
+			System::Double get()
+			{
+				return zoom;
+			}
+			void set(System::Double value)
+			{
+				zoom = value;
+			}
 		}
-	}
-	property System::Double Zoom
-	{
-		System::Double get()
+		property System::Boolean NoFrames
 		{
-			return zoom;
+			System::Boolean get()
+			{
+				return noFrames;
+			}
+			void set(System::Boolean value)
+			{
+				noFrames = value;
+			}
 		}
-		void set(System::Double value)
+		property System::Boolean ComplexMode
 		{
-			zoom = value;
+			System::Boolean get()
+			{
+				return complexMode;
+			}
+			void set(System::Boolean value)
+			{
+				complexMode = value;
+			}
 		}
-	}
-	property System::Boolean NoFrames
-	{
-		System::Boolean get()
+		property System::Boolean HtmlLinks
 		{
-			return noFrames;
+			System::Boolean get()
+			{
+				return htmlLinks;
+			}
+			void set(System::Boolean value)
+			{
+				htmlLinks = value;
+			}
 		}
-		void set(System::Boolean value)
+		property System::Boolean IgnoreImages
 		{
-			noFrames = value;
+			System::Boolean get()
+			{
+				return ignoreImages;
+			}
+			void set(System::Boolean value)
+			{
+				ignoreImages = value;
+			}
 		}
-	}
-	property System::Boolean ComplexMode
-	{
-		System::Boolean get()
+		property System::Boolean OutputHiddenText
 		{
-			return complexMode;
+			System::Boolean get()
+			{
+				return outputHiddenText;
+			}
+			void set(System::Boolean value)
+			{
+				outputHiddenText = value;
+			}
 		}
-		void set(System::Boolean value)
+		property System::String ^EncodeName
 		{
-			complexMode = value;
+			System::String ^get()
+			{
+				return encName;
+			}
+			void set(System::String ^ value)
+			{
+				encName = value;
+			}
 		}
-	}
-	property System::Boolean HtmlLinks
-	{
-		System::Boolean get()
+		property System::String ^ImageExtension
 		{
-			return htmlLinks;
+			System::String ^get()
+			{
+				return imgExt;
+			}
+			void set(System::String ^ value)
+			{
+				imgExt = value;
+			}
 		}
-		void set(System::Boolean value)
-		{
-			htmlLinks = value;
-		}
-	}
-	property System::Boolean IgnoreImages
-	{
-		System::Boolean get()
-		{
-			return ignoreImages;
-		}
-		void set(System::Boolean value)
-		{
-			ignoreImages = value;
-		}
-	}
-	property System::Boolean OutputHiddenText
-	{
-		System::Boolean get()
-		{
-			return outputHiddenText;
-		}
-		void set(System::Boolean value)
-		{
-			outputHiddenText = value;
-		}
-	}
-	property System::String ^EncodeName
-	{
-		System::String ^get()
-		{
-			return encName;
-		}
-		void set(System::String ^ value)
-		{
-			encName = value;
-		}
-	}
-	property System::String ^ImageExtension
-	{
-		System::String ^get()
-		{
-			return imgExt;
-		}
-		void set(System::String ^ value)
-		{
-			imgExt = value;
-		}
-	}
-};
+	};
+}
